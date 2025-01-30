fb2cbabc0112845ba825924fae8b13e698f203fa
vladilen11 <vladilenaksana@163.com>
2025-01-31 00:13:17 +0800

    update sui-indexer version

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
-export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0xb88bbf67ecf24ab68f609aa5602d18a31b4234fc4db2c19955772eac75361f97'
-export const SCHEMA_ID = '0x18e7cd9e1ecb7159a0b1a3d7d00d889da081d84f261d46724f6796da1cdefbc2'
+export const NETWORK: NetworkType = 'testnet';
+export const PACKAGE_ID = '0x75be74769c251892d759a330d383995f43da777380c601eeeb3d5a987d12e874'
+export const SCHEMA_ID = '0xece9e233708c41e25407c3e53ad3289069bf8e8c6d5b2b8a6337303c0d95c129'
